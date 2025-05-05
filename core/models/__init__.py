99598b82767515cb0e6439e1b3835dfe1ff3e453
LucasDeSouza <lucasdesouza57@gmail.com>
2025-05-05 20:14:54 -0300

    Feat:"criação da tabela modelo"

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,4 +1,5 @@
 from .user import User
 from .acessorios import Acessorios
 from .cor import Cor
+from .modelo import Modelo
 
--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -1,3 +1,4 @@
 from .user import UserSerializer
 from .acessorios import AcessoriosSerializer
 from .cor import CorSerializer
+from .modelo import ModeloSerializer
